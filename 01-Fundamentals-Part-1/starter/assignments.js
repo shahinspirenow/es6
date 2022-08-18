b53c2c3f5a7f6d11023aec1769b0a4744192eda6
shahinspirenow <shah@inspirenow.com.my>
2022-08-18 17:18:04 +0800

    Practical Lesson 1
    10. Values and Variables
    11. Practice Assignments
    12. Data Types

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,13 @@
+let country = 'Malaysia';
+let continent = 'Asia';
+let population = 33.08;
+
+console.log(country, continent, population)
+
+let isIsland = true;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,48 @@
+/*
+let js = 'amazing';
+if (js === 'amazing') alert('Javascript is FUN');
+
+40 + 8 + 23 - 18;
+console.log(40 + 8 + 23 - 18);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda";
+let first_name = 'jonas';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(first_name);
+
+// illegal variable = &, start with number 3years, new as reserved in javascript, never use CAPS.
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+console.log(myFirstJob);
+*/
+
+// Boolean
+// Declare Value
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'jonas');
+
+// Change Value
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
