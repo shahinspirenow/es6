4ba50914a35735bf8a9bd4b5ed4ec7bf6bb36a7b
Shah <shah@inspirenow.com.my>
2022-08-31 23:47:56 +0800

    Practical Lesson 10

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -137,4 +137,59 @@ console.log(friends[friends.length - 1])
 
 friends[2] = 'Jay';
 console.log(friends);
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+console.log(age1, age2, age3)
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])]
+console.log(ages)
+
+
+
+//ADD ELEMENT
+// Push is function that called to attached with array at last
+const friends = [`Michael`, `Steven`, `Peter`];
+const newLength = friends.push('Jay')
+console.log(friends)
+console.log(newLength)
+// Unshift is function that called to attached with array at first
+friends.unshift('John');
+console.log(friends)
+
+//REMOVE ELEMENT
+friends.pop();
+const popped = friends.pop();
+console.log(popped)
+console.log(friends)
+
+friends.shift();
+console.log(friends)
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+//True or False
+friends.push(23);
+console.log(friends.includes('Steven'))
+console.log(friends.includes('Bob'))
+console.log(friends.includes(23))
+
+if (friends.includes('Steven')) {
+    console.log('You have a friend called Steven')
+}
 */
