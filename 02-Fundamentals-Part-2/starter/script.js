0039b1513815f409c3a95ad91a84b9088b7b8225
shahinspirenow <shah@inspirenow.com.my>
2022-09-05 12:27:35 +0800

    Practical test with Syafiq

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -194,6 +194,8 @@ if (friends.includes('Steven')) {
 }
 */
 /*
+
+
 Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
 
 1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
@@ -211,13 +213,24 @@ GOOD LUCK 😀
 // Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules 
 //above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
 
+*/
 const calcTip = function (bill) {
-    const bill = [125, 555, 44];
-    bill <= 30 && bill >= 300 ? bill * 0.15 : bill * 0.20;
-    console.log(bill)
+    //const bill = [125, 555, 44];
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+    //console.log(bill)
 }
-console.log(calcTip)
+console.log(calcTip(100))
 
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+console.log(tips)
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+console.log(total)
+
+
+
+/*
 function calcTip(bill) {
     const bill = 100;
     if biil <= 30 && bill >= 300 ? bill * 0.15 : bill * 0.20;
